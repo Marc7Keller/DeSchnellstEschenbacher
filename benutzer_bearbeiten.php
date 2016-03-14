4971507f81e292fe7323267886f9ea4fc5906729
DomeAlonso <dominiquealonsof1@gmail.com>
2016-03-14 11:18:49 +0100

    Code formatierung

--- benutzer_bearbeiten.php
+++ benutzer_bearbeiten.php
@@ -1,118 +1,53 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-	<title>Administration - Benutzer bearbeiten</title>
-	<link rel="stylesheet" href="_css/style.css" type="text/css">
-	<link rel="stylesheet" href="_css/style_benutzer.css" type="text/css">
-	
-	
-	
-</head>
+    <head>
+        <title>Administration - Benutzer bearbeiten</title>
+        <link rel="stylesheet" href="_css/style.css" type="text/css">
+        <link rel="stylesheet" href="_css/style_benutzer.css" type="text/css">
+    </head>
 
-<body>
+    <body>
 
-	<div id="sitediv">
-		
-			<a href="index.php"><img id="scdiemberg_logo" src="_img/sportclubdiemberg_logo_klein.png"/></a>
-			<a href="index.php"><img id="deschnellsteschenbacher_logo" src="_img/deschnellsteschenbacher_logo_klein.png"/></a>
-			
-			<nav id="nav">
-			<ul>
-				<li class="nav1">
-					 <a href="zeiten_erfassen.php">Zeiten erfassen</a>
-				</li>
-			
-				<li class="nav2">
-					<a href="#">Auswertungen</a>
-					<ul>
-						<li><a href="#">Das ist ein Test</a></li>
-						<li><a href="#">Das ist noch ein Test</a></li>
-						<li><a href="#">Unterpunkt3</a></li>
-					</ul>
-				</li>
-				
-				<li class="nav3">
-					<a href="#">Administration</a>
-					<ul>
-						<li><a href="#">Teilnehmerverwaltung</a>
-							<ul>
-								<li><a href="neuer_teilnehmer.php">Neuer Teilnehmer</a></li>
-								<li><a href="teilnehmer_bearbeiten.php">Teilnehmer bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Personenverwaltung</a>
-							<ul>
-								<li><a href="neue_person.php">Neue Person</a></li>
-								<li><a href="person_bearbeiten.php">Person bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Kategorieverwaltung</a>
-							<ul>
-								<li><a href="neue_kategorie.php">Neue Kategorie</a></li>
-								<li><a href="kategorie_bearbeiten.php">Kategorie bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Klassenverwaltung</a>
-							<ul>
-								<li><a href="neue_klasse.php">Neue Klasse</a></li>
-								<li><a href="klasse_bearbeiten.php">Klasse bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Lehrerverwaltung</a>
-							<ul>
-								<li><a href="neuer_lehrer.php">Neuer Lehrer</a></li>
-								<li><a href="lehrer_bearbeiten.php">Lehrer bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Anlassverwaltung</a>
-							<ul>
-								<li><a href="neuer_anlass.php">Neuer Anlass</a></li>
-								<li><a href="anlass_bearbeiten.php">Anlass bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Benutzerverwaltung</a>
-							<ul>
-								<li><a href="neuer_benutzer.php">Neuer Benutzer</a></li>
-								<li><a href="benutzer_bearbeiten.php">Benutzer bearbeiten</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				
-			</ul>
-		</nav>
-		
-		<div id="content">
-		
-		<h1 id="site_title">Benutzer bearbeiten</h1>
-		
-		<form id="form_verwaltung" action="" method="POST">
-			</br><p style="font-size: 11px;">Felder mit * markiert sind Pflichtfelder</p></br>
-			<label style="font-weight: bold;">Benutzer:</label>
-										<select  id="benutzer_combo" type="text" name="benutzer_combo" size="1">
-											<option>Admin</option>
-											<option>Marc.Keller</option>
-										</select></br></br>
-			Benutzername:*				<input id="benutzername" type="text" name="banutzername"/></br>
-			Passwort:*					<input  id="passwort" type="password" name="passwort"/></br>
-			Passwort wiederholen:*		<input  id="passwort_wdh" type="password" name="passwort_wdh"/></br></br>
-			
-			 <p style="font-weight: bold">Wählen Sie die gewünschten Berechtigungen:</p></br>
-				<input type="checkbox" name="berechtigungen" value="zeit_erfassen"> Zeit erfassen</br> 
-				<input type="checkbox" name="berechtigungen" value="teilnehmer_erfassen"> Teilnehmer erfassen</br> 
-				<input type="checkbox" name="berechtigungen" value="benutzer_hinzufuegen"> Benutzer hinzufügen</br>
-				<input type="checkbox" name="berechtigungen" value="alle_rechte"> Alle Rechte (Administrator)</br></br>
-										<input id="speichern_button"type="submit" name="submit" value="Speichern"/>
-		</form>
-		
-		</div>
-		
-		<div id="footer">
-		</div>
-	
-	
-	</div>
-</body>
+        <div id="sitediv">
+
+            <a href="index.php"><img id="scdiemberg_logo" src="_img/sportclubdiemberg_logo_klein.png"/></a>
+            <a href="index.php"><img id="deschnellsteschenbacher_logo" src="_img/deschnellsteschenbacher_logo_klein.png"/></a>
+
+            <?php
+            include("php/config.php");
+            ?>
+
+            <div id="content">
+
+                <h1 id="site_title">Benutzer bearbeiten</h1>
+
+                <form id="form_verwaltung" action="" method="POST">
+                    </br><p style="font-size: 11px;">Felder mit * markiert sind Pflichtfelder</p></br>
+                <label style="font-weight: bold;">Benutzer:</label>
+                <select  id="benutzer_combo" type="text" name="benutzer_combo" size="1">
+                    <option>Admin</option>
+                    <option>Marc.Keller</option>
+                </select></br></br>
+                Benutzername:*				<input id="benutzername" type="text" name="banutzername"/></br>
+                Passwort:*					<input  id="passwort" type="password" name="passwort"/></br>
+                Passwort wiederholen:*		<input  id="passwort_wdh" type="password" name="passwort_wdh"/></br></br>
+
+                <p style="font-weight: bold">Wählen Sie die gewünschten Berechtigungen:</p></br>
+                <input type="checkbox" name="berechtigungen" value="zeit_erfassen"> Zeit erfassen</br> 
+                <input type="checkbox" name="berechtigungen" value="teilnehmer_erfassen"> Teilnehmer erfassen</br> 
+                <input type="checkbox" name="berechtigungen" value="benutzer_hinzufuegen"> Benutzer hinzufügen</br>
+                <input type="checkbox" name="berechtigungen" value="alle_rechte"> Alle Rechte (Administrator)</br></br>
+                <input id="speichern_button"type="submit" name="submit" value="Speichern"/>
+                </form>
+
+                </div>
+
+                <div id="footer">
+                </div>
+
+
+        </div>
+    </body>
 
 </html>
--- neue_kategorie.php
+++ neue_kategorie.php
@@ -28,71 +28,9 @@
 			<a href="index.php"><img id="scdiemberg_logo" src="_img/sportclubdiemberg_logo_klein.png"/></a>
 			<a href="index.php"><img id="deschnellsteschenbacher_logo" src="_img/deschnellsteschenbacher_logo_klein.png"/></a>
 			
-			<nav id="nav">
-			<ul>
-				<li class="nav1">
-					 <a href="zeiten_erfassen.php">Zeiten erfassen</a>
-				</li>
-			
-				<li class="nav2">
-					<a href="#">Auswertungen</a>
-					<ul>
-						<li><a href="#">Das ist ein Test</a></li>
-						<li><a href="#">Das ist noch ein Test</a></li>
-						<li><a href="#">Unterpunkt3</a></li>
-					</ul>
-				</li>
-				
-				<li class="nav3">
-					<a href="#">Administration</a>
-					<ul>
-						<li><a href="#">Teilnehmerverwaltung</a>
-							<ul>
-								<li><a href="neuer_teilnehmer.php">Neuer Teilnehmer</a></li>
-								<li><a href="teilnehmer_bearbeiten.php">Teilnehmer bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Personenverwaltung</a>
-							<ul>
-								<li><a href="neue_person.php">Neue Person</a></li>
-								<li><a href="person_bearbeiten.php">Person bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Kategorieverwaltung</a>
-							<ul>
-								<li><a href="neue_kategorie.php">Neue Kategorie</a></li>
-								<li><a href="kategorie_bearbeiten.php">Kategorie bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Klassenverwaltung</a>
-							<ul>
-								<li><a href="neue_klasse.php">Neue Klasse</a></li>
-								<li><a href="klasse_bearbeiten.php">Klasse bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Lehrerverwaltung</a>
-							<ul>
-								<li><a href="neuer_lehrer.php">Neuer Lehrer</a></li>
-								<li><a href="lehrer_bearbeiten.php">Lehrer bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Anlassverwaltung</a>
-							<ul>
-								<li><a href="neuer_anlass.php">Neuer Anlass</a></li>
-								<li><a href="anlass_bearbeiten.php">Anlass bearbeiten</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Benutzerverwaltung</a>
-							<ul>
-								<li><a href="neuer_benutzer.php">Neuer Benutzer</a></li>
-								<li><a href="benutzer_bearbeiten.php">Benutzer bearbeiten</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				
-			</ul>
-		</nav>
+			<?php
+            include("php/config.php");
+            ?>
 		
 		<div id="content">
 		
