3f8557a451a2d6eb4a864c61ed7e4ce09439cb42
DomeAlonso <dominiquealonsof1@gmail.com>
2016-03-14 11:19:37 +0100

    Code formatierung

---
+++ includes/navigation.php
@@ -0,0 +1,59 @@
+<nav id="nav">
+			<ul>
+				<li class="nav1">
+					 <a href="zeiten_erfassen.php">Zeiten erfassen</a>
+				</li>
+			
+				<li class="nav2">
+					<a href="#">Auswertungen</a>
+					<ul>
+						<li><a href="#">Das ist ein Test</a></li>
+						<li><a href="#">Das ist noch ein Test</a></li>
+						<li><a href="#">Unterpunkt3</a></li>
+					</ul>
+				</li>
+				
+				<li class="nav3">
+					<a href="#">Administration</a>
+					<ul>
+						<li><a href="#">Teilnehmerverwaltung</a>
+							<ul>
+								<li><a href="neuer_teilnehmer.php">Neuer Teilnehmer</a></li>
+								<li><a href="teilnehmer_bearbeiten.php">Teilnehmer bearbeiten</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Personenverwaltung</a>
+							<ul>
+								<li><a href="neue_person.php">Neue Person</a></li>
+								<li><a href="person_bearbeiten.php">Person bearbeiten</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Kategorieverwaltung</a>
+							<ul>
+								<li><a href="neue_kategorie.php">Neue Kategorie</a></li>
+								<li><a href="kategorie_bearbeiten.php">Kategorie bearbeiten</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Klassenverwaltung</a>
+							<ul>
+								<li><a href="neue_klasse.php">Neue Klasse</a></li>
+								<li><a href="klasse_bearbeiten.php">Klasse bearbeiten</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Anlassverwaltung</a>
+							<ul>
+								<li><a href="neuer_anlass.php">Neuer Anlass</a></li>
+								<li><a href="anlass_bearbeiten.php">Anlass bearbeiten</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Benutzerverwaltung</a>
+							<ul>
+								<li><a href="neuer_benutzer.php">Neuer Benutzer</a></li>
+								<li><a href="benutzer_bearbeiten.php">Benutzer bearbeiten</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				
+			</ul>
+		</nav>
